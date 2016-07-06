0125cc1c326f8596cf7c45db81a98c0dfacc02e8
dnguyensr <danielnguyensr@gmail.com>
2016-07-05 19:50:18 -0700

    Add Procfile and heroku deployment configs

--- config/puma.rb
+++ config/puma.rb
@@ -4,9 +4,10 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
-
+preload_app!
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
@@ -42,6 +43,10 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # on_worker_boot do
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
