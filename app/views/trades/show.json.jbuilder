a6304c98cd0fd8439b62f8d6e06f10b995baf2af
dnguyensr <danielnguyensr@gmail.com>
2016-08-08 23:36:14 -0700

    Scaffold trades

---
+++ app/views/trades/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @trade, :id, :owner_id, :item, :quantity, :for_item, :for_cash, :cost, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :trades
   resources :vouchers
   root 'posts#index'
   resources :posts
